1c3b88704d3d1dde5b6cd7b21a3a2466418178a5
Shubham Manohar <shubhammanohar20@gmail.com>
2026-05-25 17:49:34 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(cors({}));
+app.use(express.json());
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}`);
+});
